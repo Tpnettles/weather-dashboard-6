285ff7dc7c57975a8db5d208d5c3b7fa31e51898
Tyree Nettles <nettles.code@gmail.com>
2021-05-19 23:18:34 -0500

    changes made on 5/19

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# weather-dashboard
+When a user reaches the page, they are able to enter any city or state in the U.S and retrieve accurate temperatures.
+
+The search button must be pressed to display results.
+
+Common cities are displayed on the left side of the screen, in button form.
+
+Click the City's associated button to display results. 
+
+
---
+++ main.js
@@ -0,0 +1,76 @@
+//Variables
+let search = document.getElementById('city-search')
+let submitButton = document.getElementById('submit-button')
+let miami = document.getElementById('miami')
+let houston = document.getElementById('houston')
+let atlanta = document.getElementById('atlanta')
+let newYork = document.getElementById('new-york')
+let denver = document.getElementById('denver')
+let nashville = document.getElementById('nashville')
+let chicago = document.getElementById('chicago')
+let vegas = document.getElementById('vegas')
+let temp = document.getElementById('temp')
+let cityName = document.getElementById('city-name')
+
+
+
+//functions
+function getWeather(city){
+ $.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7aaa3785a6cb0e55044cd5cba2b25d4d`, function(data){
+ console.log(data)
+ cityName.innerHTML= `City: ${data.name}`
+ temp.innerHTML= `Temperature: ${data.main.temp}`
+
+ });
+}
+
+// getWeather('chicago');
+
+
+submitButton.addEventListener('click', event =>{
+    getWeather(search.value)
+});
+
+denver.addEventListener('click', event =>{
+     getWeather('denver')
+ });
+
+ houston.addEventListener('click', event =>{
+     getWeather('houston')
+ });
+
+ chicago.addEventListener('click', event =>{
+    getWeather('chicago')
+ })
+
+ nashville.addEventListener('click', event =>{
+    getWeather('nashville')
+})
+
+vegas.addEventListener('click', event =>{
+    getWeather('vegas')
+})
+
+miami.addEventListener('click', event =>{
+    getWeather('miami')
+})
+
+atlanta.addEventListener('click', event =>{
+    getWeather('atlanta')
+})
+
+indianapolis.addEventListener('click', event =>{
+    getWeather('indianapolis')
+})
+
+
+//  init
+// check local storage for the key (cities) if present assign each city to button in the left panel
+
+
+
+
+
+
+//Events
+
